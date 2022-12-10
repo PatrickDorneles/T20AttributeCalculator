405d8197f55579518c36379f35865f67c1e66477
Patrick Dorneles <pdorneles.dev@gmail.com>
2022-12-09 23:32:26 -0300

    adding description text below the application

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,14 +18,63 @@ const Home: NextPage = () => {
       <main >
         <div className="background-pattern h-screen flex flex-col justify-center items-center gap-4">
           <Logo className="h-24 w-24" />
-          <h1 className="text-white text-4xl font-bold font-display">T20 Attribute Calculator</h1>
-
+          <div className="flex flex-col items-center justify-center">
+            <h1 className="text-white text-4xl font-bold font-display">T20 Attribute Calculator</h1>
+            <pre className="text-white font-normal">This calculator is for Game of The Year Edition only</pre>
+          </div>
           <div className="flex items-center ">
+
             
           </div>
 
         </div>
-        <section>
+        <section className="bg-black py-10 px-4 text-white">
+          <article className="flex flex-col gap-4">
+            <h1 className="text-4xl mt-12">What is this application?</h1>
+            <p>
+              It is a calculator to calculate attributes to characters for
+              a Brazilian &quot;TTRPG&quot; (Table Top Role Playing Game) 
+              called Tormenta20, very famous on Brazil.
+              Its system is an adaptation from D20 SRD with his 
+              own scenario, a medieval fantasy with some inspirations
+              on anime and with some Lovecraftian-like creatures.
+            </p>
+            <p>
+              In this universe called Arton there is an phenomena called 
+              Tormenta, an corrupted storm from another dimension that brings 
+              with it corrupted demon creatures called Lefeu, this storm
+              also affect humanoids corrupting them and transforming its 
+              bodies.
+            </p>
+            <p>
+              Also there is an importance for the number 20 on this RPG, that
+              came from the 20 years since the first release of the scenario,
+              the 20 major gods that control this world and the 20-sided 
+              die (a classic and important dice for the game), all this led 
+              its writers to name the most recent version as <b>Tormenta 20</b>.
+            </p>
+          </article>
+
+          <article className="flex flex-col gap-4">
+            <h1 className="text-4xl mt-8">How it works?</h1>
+            <p>
+              On Tormenta20 &quot;Edição Jogo do Ano&quot; the character&apos;s
+              attributes are the same from D&D3.5: Strength, Dexterity, Constitution,
+              Intelligence, Wisdom and Charisma, but theres no modifier mechanic, because,
+              on T20 the attribute value works as the classical attribute modifier.
+            </p>
+            <p>
+              All races modify the attribute in some way, some give a bonus to some attributes,
+              others can subtract values from certain attributes and others can give you
+              the choice to choose what attributes do you wanna buff.
+            </p>
+            <p>
+              The book mentions 2 ways to get your attributes, one of them is
+              rolling a 4d6 taking the 3 highest and looking into a table to
+              define the attribute, the other one is a <b>points system</b>,
+              which this calculator is made for.
+            </p>
+          </article>
           
         </section>
 
