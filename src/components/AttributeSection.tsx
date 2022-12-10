68b2253878d4db3067b88ebb002912bb9cea4450
Patrick Dorneles <pdorneles.dev@gmail.com>
2022-12-10 00:00:40 -0300

    changing behavior and bg

--- src/components/AttributeSection.tsx
+++ src/components/AttributeSection.tsx
@@ -1,1 +1,3 @@
-export const Attribute = () => (<></>)
+export const AttributeSection = () => (<>
+
+</>)
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,4 +1,19 @@
 
+const bgHero = require("tailwind-heropatterns")({
+  // as per tailwind docs you can pass variants
+  variants: [],
+  patterns: ["topography"],
+  colors: {
+    default: "#000",
+  },
+
+  // The foreground opacity
+  opacity: {
+    default: "0.4",
+    "100": "1.0"
+  }
+});
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
@@ -10,5 +25,5 @@ module.exports = {
     extend: {
     },
   },
-  plugins: [],
+  plugins: [bgHero],
 };
