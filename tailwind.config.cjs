cfdc7be54d1874867c347fdeaf776e0e90456c21
Patrick Dorneles <pdorneles.dev@gmail.com>
2022-12-09 22:43:00 -0300

    feat: adding project

---
+++ tailwind.config.cjs
@@ -0,0 +1,14 @@
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    
+    fontFamily: {
+      'display': ['Hammersmith One']
+    },
+    extend: {
+    },
+  },
+  plugins: [],
+};
