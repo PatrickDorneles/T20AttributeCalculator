6164216cb3ba234e44449e9ce318bf523289ca34
Patrick Dorneles <pdorneles.dev@gmail.com>
2022-12-11 03:38:41 -0300

    adding md description

---
+++ public/md/description.en.md
@@ -0,0 +1,66 @@
+# What is this application?
+
+It is a calculator to help you building attributes to characters from
+a Brazilian &quot;TTRPG&quot; (Table Top Role Playing Game)
+called Tormenta20, very famous on Brazil.
+Its system is an adaptation from D20 SRD with his 
+own scenario, a medieval fantasy with some inspirations
+on anime.
+
+In this universe called Arton there is an phenomena called 
+Tormenta, an corrupted storm from another dimension that brings 
+with it corrupted demon creatures called Lefeu, this storm
+also affect humanoids corrupting them and transforming its 
+bodies.
+
+Also there is an importance for the number 20 on this RPG, that
+came from the 20 years since the first release of the scenario,
+the 20 major gods that control this world and the 20-sided 
+die (a classic and important dice for the game), all this led 
+its writers to name the most recent version as **Tormenta 20**.
+
+![Capa T20](./assets/t20-basico-jogo-do-ano-removebg-preview.png)
+Cover of Tormenta 20 Edição Jogo do Ano  
+Image from [Jambo Editora](https://jamboeditora.com.br/produto/tormenta20-edicao-jogo-do-ano/)
+
+# How it works?
+
+On Tormenta20 &quot;Edição Jogo do Ano&quot; the character&apos;s
+attributes are the same from D&D3.5: Strength, Dexterity, Constitution,
+Intelligence, Wisdom and Charisma, but theres no modifier mechanic, 
+because on T20 the attribute value works as the classical 
+attribute modifier.
+
+All races modify the attribute in some way, some give a bonus to some attributes,
+others can subtract values from certain attributes and others can give you
+the choice to choose what attributes do you wanna buff.
+
+The book mentions 2 ways to get your attributes, one of them is
+rolling a 4d6 taking the 3 highest and looking into the table to
+define the attribute, the other one is a **points system**,
+which this calculator is made for. In this system you have 10
+points to define how to distribute your attributes, each value
+has a cost and set of values to roll for the dice system.
+
+| Attribute | Cost | Roll |
+|:---:|:---:|:---:|
+| -2 | - | 7 or less |
+| -1 | -1 point | 8-9 |
+| 0 | 0 point | 10-11 |
+| 1 | 1 point | 12-13 |
+| 2 | 2 points | 14-15 |
+| 3 | 4 points | 16-17 |
+| 4 | 7 points | 18 |
+
+# Who made this?
+Hi, my name is Patrick Dorneles and I am a RPG player and DM, software developer and aspiring
+game developer, I love RPG and play on Arton since 2016 where I discovered the Tormenta RPG
+book, and it was always awesome, now with this new version I liked a lot this new attribute
+system but since there was many calculators for the legacy D&D inspired modifier system but
+no calculators for this one I feeled that I could do it. If you can I will love any help or
+feedback about this project, that you can find on [this GitHub repo](https://github.com/PatrickDorneles/T20AttributeCalculator).
+
+If you would like to contact me you can reach me through these links:  
+[![LinkedIn](https://img.shields.io/badge/-Linkedin-1467C3?style=for-the-badge&logo=Linkedin&logoColor=white)](https://www.linkedin.com/public-profile/in/patrick-dorneles-922632162/)
+[![Facebook](https://img.shields.io/badge/-Facebook-1876F3?style=for-the-badge&logo=Facebook&logoColor=white)](https://www.facebook.com/people/Patrick-Dorneles/100072043064054/)
+[![Facebook](https://img.shields.io/badge/-GitHub-333333?style=for-the-badge&logo=GitHub&logoColor=white)](https://github.com/PatrickDorneles)
--- src/components/layout/DescriptionLayout.tsx
+++ src/components/layout/DescriptionLayout.tsx
@@ -1,49 +1,30 @@
-export const DescriptionLayout = () => <section className="bg-black py-10 px-4 text-white">
-    <article className="flex flex-col gap-4">
-    <h1 className="text-4xl mt-12 font-bold">What is this application?</h1>
-    <p>
-        It is a calculator to help you building attributes to characters from
-        a Brazilian &quot;TTRPG&quot; (Table Top Role Playing Game)
-        called Tormenta20, very famous on Brazil.
-        Its system is an adaptation from D20 SRD with his 
-        own scenario, a medieval fantasy with some inspirations
-        on anime and with some Lovecraftian-like creatures.
-    </p>
-    <p>
-        In this universe called Arton there is an phenomena called 
-        Tormenta, an corrupted storm from another dimension that brings 
-        with it corrupted demon creatures called Lefeu, this storm
-        also affect humanoids corrupting them and transforming its 
-        bodies.
-    </p>
-    <p>
-        Also there is an importance for the number 20 on this RPG, that
-        came from the 20 years since the first release of the scenario,
-        the 20 major gods that control this world and the 20-sided 
-        die (a classic and important dice for the game), all this led 
-        its writers to name the most recent version as <b>Tormenta 20</b>.
-    </p>
-    </article>
+import { useEffect, useState } from 'react'
+import ReactMarkdown from 'react-markdown'
+import remarkGfm from 'remark-gfm'
 
-    <article className="flex flex-col gap-4">
-    <h1 className="text-4xl mt-8 font-bold">How it works?</h1>
-    <p>
-        On Tormenta20 &quot;Edição Jogo do Ano&quot; the character&apos;s
-        attributes are the same from D&D3.5: Strength, Dexterity, Constitution,
-        Intelligence, Wisdom and Charisma, but theres no modifier mechanic, 
-        because on T20 the attribute value works as the classical 
-        attribute modifier.
-    </p>
-    <p>
-        All races modify the attribute in some way, some give a bonus to some attributes,
-        others can subtract values from certain attributes and others can give you
-        the choice to choose what attributes do you wanna buff.
-    </p>
-    <p>
-        The book mentions 2 ways to get your attributes, one of them is
-        rolling a 4d6 taking the 3 highest and looking into a table to
-        define the attribute, the other one is a <b>points system</b>,
-        which this calculator is made for.
-    </p>
-    </article>
-</section>
+
+export const DescriptionLayout = () => {
+    const [text, setText] = useState('')
+
+    useEffect(() => {
+        fetch('./md/description.en.md')
+            .then(res => res.text())
+            .then(setText)
+    })
+
+    return <section className="bg-black py-10 px-4 text-white">
+        <ReactMarkdown 
+            components={{
+                'h1': ({ children }) => <h1 className='text-3xl font-bold' >{children}</h1>,
+                'table': ({ children }) => <table className='w-fit '>{children}</table>,
+                'tr': ({ children }) => <tr className=''>{ children }</tr>,
+                'td': ({ children }) => <td className='border-white border-solid border-2 py-1 px-2' >{ children }</td>,
+                'th': ({ children }) => <th className='border-white border-solid border-2 py-1 px-2' >{ children }</th>,
+                'a': ({ children, href }) => <a className='text-blue-500 cursor-pointer' href={href} >{ children }</a>
+            }}
+            className="flex flex-col gap-4" 
+            remarkPlugins={[remarkGfm]} >
+            {text}
+        </ReactMarkdown>
+    </section>
+}
